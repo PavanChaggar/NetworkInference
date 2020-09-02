465efdeb1a378f0a79762a7927466ca75dad3fc5
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-09-02 14:08:28 +0100

    adding setup.py with requirements and __ini__ in src. Creating first test file for development on networks

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='netwin',
+    version='0.1',
+    description='A toolbox for performing modelling and inference on networks',
+    license='MIT license',
+    maintainer='Pavan Chaggar',
+    maintainer_email='pavanjit.chaggar@maths.ox.ac.uk',
+    include_package_data=True,
+    packages = find_packages(include=('src', 'src.*')),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'networkx'
+    ],
+)
---
+++ tests/test_networks.py
@@ -0,0 +1,8 @@
+"""File containg test for classes/functions in src/networks.py
+"""
+
+import unittest
+import netwin as nw 
+
+class TestConnectome(unittest.TestCase):
+    pass
