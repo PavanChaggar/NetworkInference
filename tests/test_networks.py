c2617f9c43573109bad45a3f5ab3b47eef3e04f2
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-09-02 17:00:38 +0100

    adding test for reading adjacency matrix from csv. graphml not implemented.

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -2,7 +2,20 @@
 """
 
 import unittest
-import netwin as nw 
+import os
+import netwin as nw
 
-class TestConnectome(unittest.TestCase):
-    pass
+
+class Test_Network(unittest.TestCase):
+    root_dir = os.path.split(os.path.dirname(__file__))[0]
+    graph_path = os.path.join(root_dir, 'data/brain_networks/scale1.csv')
+
+    def test_adjacency_matrix(self):
+        assert self.graph_path.endswith('.csv')
+         
+        A = nw.adjacency_matrix(self.graph_path)
+        n = len(A) 
+        assert A.shape == (n,n)
+
+    def test_degree_matrix(self): 
+        pass
