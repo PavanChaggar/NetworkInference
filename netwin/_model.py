bcae4b56ae22f124a30a93a32f451c472f0670c2
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-09-03 16:04:15 +0100

    updating _models.py

--- netwin/_model.py
+++ netwin/_model.py
@@ -1,2 +1,15 @@
 """ script containing class and functions for modelling, integrating etc
 """ 
+
+def model(model: str): 
+    if model == 'diffusion': 
+        return network_diffusion
+
+def network_diffusion(u0, params, t):
+    p = u0
+    L, k = params
+    du = k * (-L @ p)
+    return du
+
+def solve(model, u0, params, t):
+    return odeint(model, u0, t, args=(params,))
