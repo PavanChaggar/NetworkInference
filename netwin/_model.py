dd6c005d1c9f623f2acc445c5f83f1cf41f37943
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-09-04 17:11:29 +0100

    adding docstrings and comments to code. Removing redundant lines and print statements

--- netwin/_model.py
+++ netwin/_model.py
@@ -4,9 +4,19 @@ import netwin as nw
 from scipy.integrate import odeint
 
 class Model(object): 
-    
+    """Model class for the easy implementation of network models
+    """
     def __init__(self, network_path: str, model_name: str):
-        
+        """initialise class with network and model 
+        args:
+          filename : str
+                     string to the location of a csv containing an adjacency matrix
+        model_name : str 
+                     string containing the model user wishes to initialised 
+                     options: 
+                        - 'network_diffusion' 
+                           du = k*(L @ p)
+        """
         self.filename = network_path
         self.which_model = model_name
 
@@ -17,6 +27,17 @@ class Model(object):
         self.f, self.which_model = self._models(model_choice = self.which_model)
 
     def _models(self, model_choice: str): 
+        """HoF to set model variable based on user choice 
+           Presently only network diffusion is implemented 
+        args: 
+        model choice : str 
+                       string containg the moder user wishes to initialise
+        returns:
+                   f : function
+                       function imported from model library implementing desired network model
+        model_choice : str 
+                       return string containing the function set to f
+        """
         if model_choice == 'network_diffusion': 
             f = nw.network_diffusion
             return f, model_choice
@@ -24,4 +45,16 @@ class Model(object):
             pass 
 
     def solve(self, u0, t, params):
+        """Function to use odeint to sovle network models 
+        args: 
+               u0 : array  
+                    array containing intial conditions 
+                t : arrau 
+                    arrau containing time steps at which to evaluate model   
+           params : array 
+                    array containing parameter values
+        returns: 
+                u : array 
+                    solution to differential equation at times t 
+        """
         return odeint(self.f, u0, t, args=(params,))
--- netwin/_model_library.py
+++ netwin/_model_library.py
@@ -1,6 +1,18 @@
 """ Scipt containing different network models 
 """
 def network_diffusion(u0, t, params):
+    """Function to implement the network diffusion model 
+    args: 
+        u0 : array  
+             array containing intial conditions 
+         t : arrau 
+             arrau containing time steps at which to evaluate model   
+    params : array 
+             array containing parameter values
+    returns: 
+          u : array 
+              solution to differential equation at times t 
+    """
     p = u0
     L, k = params
     du = k * (-L @ p)
