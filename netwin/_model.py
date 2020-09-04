004525c829fc0aafb4c128694e45d9535c51cf0d
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-09-04 10:08:25 +0100

    adding class structure around existing function. Fixing bug in _netwrk_diffusion

--- netwin/_model.py
+++ netwin/_model.py
@@ -1,15 +1,33 @@
 """ script containing class and functions for modelling, integrating etc
 """ 
+import netwin as nw
 
-def model(model: str): 
-    if model == 'diffusion': 
-        return network_diffusion
+class model: 
+    
+    def __init__(self,
+                 network_path: str,
+                 model_name: str)
+        
+        self.filename = network_path
+        self.which_model = model_name
 
-def network_diffusion(u0, params, t):
-    p = u0
-    L, k = params
-    du = k * (-L @ p)
-    return du
+        self.A = nw.adjacency_matrix(self.filename)
+        self.D = nw.degree_matrix(self.A)
+        self.L = nw.graph_Laplacian(A=self.A, D=self.D)
+    
+        self.f, self.which_model = _model(model_name = self.which_model)
 
-def solve(model, u0, params, t):
-    return odeint(model, u0, t, args=(params,))
+    def _model(model_name: str): 
+        if model == 'diffusion': 
+            return _network_diffusion, model_name
+        if model == 'fkpp':
+            pass 
+
+    def _network_diffusion(u0, t, params):
+        p = u0
+        L, k = params
+        du = k * (-L @ p)
+        return du
+
+    def solve(model, u0, t, params):
+        return odeint(model, u0, t, args=(params,))
