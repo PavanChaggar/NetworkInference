3f87f288be8456f27f3a4a37229634caa22fb40c
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-11-05 15:02:20 +0000

    adding all of the doc strings

--- netwin/_infer.py
+++ netwin/_infer.py
@@ -18,6 +18,25 @@ class VBProblem(object):
     parameter values and prior distribution parameter values
     """
     def __init__(self, model=None, data=None, init_means=None, priors=None):
+        """Initialise VBProblem 
+        This sets of the VBProblem class and ensures the set up is correctly set up to interface 
+        with the vb protocol. 
+        
+        args: 
+             model : Class object
+                     implementation of abstract Model class 
+              data : array, float
+                     array containing data. This should be the same shape as the output of 
+                     the generative model 
+        init_means : array, float, 
+                     vector of initial guesses for generative model parameters
+            priors : None
+                     custom priors not yet implemented
+
+        returns: 
+         VBProblem : Class object
+                     Initialised class object for which inference can be conducted
+        """
         self.__which_inference = 'VB'
 
         if not isinstance(model, Model):
@@ -31,7 +50,21 @@ class VBProblem(object):
         self.__n_params = len(init_means) - len(model.L())
 
     def __vbinferenceproblem(self, init_means, priors=None): 
-
+        """ Function to set up vb inference protocol.  
+        Sets up initial parameters and priors according to MVN distribution on model parameters 
+        and Gamma distribution on noise. 
+        args: 
+        init_means : array, float
+                     vector of initial guesses for generative model parameters
+            priors : default None
+                     Otherwise not implemented
+        
+        returns: 
+            params : multidimensional array 
+                     stores values for MVN and Gamma distribution
+            params : multidimensional array 
+                     stores values for MVN and Gamma distribution priors
+        """
         if priors == None:
             priors = self.__vbsetpriors(init_means)
         else:
@@ -48,7 +81,16 @@ class VBProblem(object):
         return params, priors
 
     def __vbsetpriors(self, init_means):
+        """ Function to set priors based on initial means
 
+        args: 
+        init_means : array, float
+                     vector of initial guesses for generative model parameters
+        
+        returns: 
+            priors : multidimensional array 
+                     stores values for MVN and Gamma distribution                     
+        """
         m0 = np.zeros_like(init_means)
         p0 = np.linalg.inv(np.eye(len(m0)) * 1e5)
 
@@ -66,10 +108,40 @@ class VBProblem(object):
         return priors
         
     def infer(self, n = 10):
+        """ Runs inference by calling from _vb.py 
+        
+        args: 
+            n : int
+                number of iterations to run vb 
+        
+        returns :
+          sol : multidimensional array
+                fitted parameters for MVN and Gamma moments
+            F : array 
+                vector array containing free energy tracking 
+        """
         if self.__which_inference == 'VB': 
             return vb(M=self.__model, data=self.__data, t=self.__t, params=self.__params, priors=self.__priors, n_params=self.__n_params, n=n)
 
-    def get(self,attribute:str):
+    def get(self, attribute:str):
+        """function to return values from hidden variables 
+        
+        args: 
+        attribute : string
+                    name of attribute one wishes to retrieve. 
+                    Options are: 
+                    'model'
+                    'data'
+                    'time'
+                    'init_means'
+                    'params'
+                    'priors'
+                    'n_params'
+                    'which'
+
+        returns: 
+        attributes[arg] : argument value
+        """
         attributes = {
              'model': self.__model,
               'data': self.__data,
