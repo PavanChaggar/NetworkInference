afe91467d2b9ac0c5b8e5e720a72ec13a8658f8d
Pavan Chaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2021-01-11 20:14:35 +0000

    updating doc strings

--- netwin/_model.py
+++ netwin/_model.py
@@ -5,7 +5,13 @@ from abc import ABC, abstractmethod
 from ._networks import *
 
 class Model(ABC): 
-    """Model class for the easy implementation of network models
+    """Abstract Base Class for implementing dynamical network models.
+
+       Parameters:
+       -----------
+       network_path (str)
+            path to a network array, .csv, or .graphml
+
     """
     def __init__(self, network_path: str):
         """initialise class with network and model 
@@ -29,80 +35,67 @@ class Model(ABC):
 
     @abstractmethod
     def f(self):
-        """Function set to be the model implemented
-        args: 
-            u0 : array  
-                array containing intial conditions 
-            t : array
-                numpy array containing time steps at which to evaluate model
-                e.g. t = np.linespace(0, 1, 100)
-        params : array 
-                array containing parameter values
-                e.g. params = [L, k]
-                    L = Graph Laplacian 
-                    k = diffusion coefficient 
-        returns: 
-            u : array 
-                solution to differential equation at times t 
+        """Differential equation function to be implemented
+
+        Parameters:
+        -----------
+        u0 (array)
+            array containing intial conditions
+        t (array)
+            numpy array containing time steps at which to evaluate model
+            e.g. t = np.linespace(0, 1, 100)
+        params (array)
+            array containing parameter values
+            e.g. params = [L, k]
+                L = Graph Laplacian
+                k = diffusion coefficient
         """
         raise NotImplementedError('This should be implemented')
 
     @abstractmethod
     def solve(self):
         """Function to use odeint to sovle network model in f
+
         args: 
-               u0 : array  
-                    array containing intial conditions 
-                t : arrau 
-                    arrau containing time steps at which to evaluate model   
-           params : array 
-                    array containing parameter values
-        returns: 
-                u : array 
-                    solution to differential equation at times t 
+        -----
+        u0 (array)
+            array containing intial conditions
+        t (array)
+            array containing time steps at which to evaluate model
+        params (array)
+           array containing parameter values
         """
         raise NotImplementedError('This should be implemented')
 
     @abstractmethod
     def forward(self):
-        """Function to describt ehf forward model
-        args: 
-               u0 : array  
-                    array containing intial conditions 
-                t : arrau 
-                    arrau containing time steps at which to evaluate model   
-           params : array 
-                    array containing parameter values
-        returns: 
-                u : array 
-                    solution to differential equation at times t 
+        """
+        Function to describt ehf forward model
+
+        args:
+        -----
+        u0 (array)
+            array containing intial conditions
+        t (array)
+            arrau containing time steps at which to evaluate model
+        params (array)
+            array containing parameter values
         """
         raise NotImplementedError('This should be implemented')
     
     def L(self):
         """Return the Laplacian matrix associated with the model.
-
-        Returns:
-            L : numpy array
-                n x n Laplacian Matrix
         """
         return self.__L
 
     def A(self):
         """Return the adjency matrix associated with the model.
-
-        Returns:
-            A : numpy array
-                n x n adjacency Matrix
         """
-        return self.__A 
+        return self.__A
     
     def D(self):
-        """Return the degree matrix associated with the model.
-
-        Returns:
-            D : numpy array
-                n x n Degree Matrix
+        """
+        Return the degree matrix associated with the model.
         """
         return self.__D 
 
--- netwin/models/_model_library.py
+++ netwin/models/_model_library.py
@@ -8,25 +8,24 @@ from scipy.integrate import odeint
 class NetworkDiffusion(Model):
     """Network Diffusion model instantiation of the Model class
 
-    Args:
-        Model : class
-                abstract base class for forward models
-         Path : str 
-                string to the csv corresponding to the network A matrix. 
+    Args
+    ----
+    Model (class)
+        abstract base class for forward models
+    Path (str)
+        string to the csv corresponding to the network A matrix.
     """
     def f(self, p, t, theta):
         """Differential equation model for network diffusion
 
-        Args:
-            p : array
-                protein concentration
-            t : array   
-                array of time points to evlulate at
-        theta : array
-                model parameters
-
-        Returns:
-            du: values of the ode model evalulate at parameters p, theta.
+        Args
+        ----
+        p (array)
+            protein concentration
+        t (array)
+            array of time points to evlulate at t
+        theta (array)
+            model parameters
         """
         k = theta
         du = k * (-self.L() @ p)
@@ -35,28 +34,25 @@ class NetworkDiffusion(Model):
     def solve(self, p, theta):
         """numerical integration of ode model
 
-        Args:
-            p : array
-                protein concentration vector
-        theta : array 
-                model parameters
-
-        Returns:
-            u : array
-                numerical solution to ode
+        Args
+        ----
+        p (array)
+            protein concentration vector
+        theta (array)
+            model parameters
         """
         return odeint(self.f, p, self.t, args=(theta,))
 
     def forward(self, u0): 
-        """forward model taking in free parameters and returning simulated 
+        """forward model taking in free parameters and returning simulated
         data
 
-        Args:
-            u0 (array): initial values for protein concentration vector and 
-            model parameters
+        Args
+        ----
 
-        Returns:
-            u: numerical solution to forward model
+        u0 (array)
+            initial values for protein concentration vector and model parameters
+
         """
         p = np.exp(u0[:-1])
         theta = u0[-1]
@@ -67,25 +63,25 @@ class NetworkDiffusion(Model):
 class NetworkFKPP(Model):
     """Network FKPP model instantiation of the Model class
 
-    Args:
-        Model : class
-                abstract base class for forward models
-         Path : str 
-                string to the csv corresponding to the network A matrix. 
+    Args
+    ----
+    Model (class)
+        abstract base class for forward models
+    Path (str)
+        string to the csv corresponding to the network A matrix.
     """
     def f(self, p, t, theta):
         """Differential equation model for network FKPP
-
+        returns array of values for the ode model
         Args:
-            p : array
-                protein concentration
-            t : array   
-                array of time points to evlulate at
-        theta : array
-                model parameters
+        -----
+        p (array)
+            protein concentration
+        t (array)
+            array of time points to evlulate at
+        theta (array)
+            model parameters
 
-        Returns:
-            du: values of the ode model evalulate at parameters p, theta.
         """
         k, a = theta
         du = k * (-self.L() @ p) + (a * p) * (1 - p)
@@ -94,28 +90,26 @@ class NetworkFKPP(Model):
     def solve(self, p, theta):
         """numerical integration of ode model
 
-        Args:
-            p : array
-                protein concentration vector
-        theta : array 
-                model parameters
+        Args
+        ----
+        p (array)
+            protein concentration vector
+        theta (array)
+            model parameters
 
-        Returns:
-            u : array
-                numerical solution to ode
         """
         return odeint(self.f, p, self.t, args=(theta,))
 
     def forward(self, u0): 
-        """forward model taking in free parameters and returning simulated 
+        """forward model taking in free parameters and returning simulated
         data
 
-        Args:
-            u0 (array): initial values for protein concentration vector and 
-            model parameters
+        Args
+        ----
 
-        Returns:
-            u: numerical solution to forward model
+        u0 (array)
+            initial values for protein concentration vector and model parameters
+
         """
         p = np.exp(u0[:-2])
         theta = np.exp(u0[-2:])
